f6158ba645dadbaa1f43b3df88b51046a360819a
Alessio Pittiglio <alessiopittiglio@icloud.com>
2025-11-06 01:45:20 +0100

    Reformat code with Black

--- mlcv_openset_segmentation/model_uncertainty.py
+++ mlcv_openset_segmentation/model_uncertainty.py
@@ -8,13 +8,13 @@ ANOMALY_ID = 13
 
 class UncertaintyModel(BaseSemanticSegmentationModel):
     def __init__(
-            self,
-            num_classes: int = 13,
-            model_name: str = 'deeplabv3_resnet50',
-            use_aux_loss: bool = True,
-            optimizer_kwargs: dict = None,
-            scheduler_kwargs: dict = None,
-            uncertainty_type='msp',
+        self,
+        num_classes: int = 13,
+        model_name: str = "deeplabv3_resnet50",
+        use_aux_loss: bool = True,
+        optimizer_kwargs: dict = None,
+        scheduler_kwargs: dict = None,
+        uncertainty_type="msp",
     ):
         super().__init__(
             num_classes=num_classes,
@@ -29,14 +29,14 @@ class UncertaintyModel(BaseSemanticSegmentationModel):
         self.all_pixel_anomaly_labels = []
 
     def _calculate_anomaly_scores(self, logits_known: torch.Tensor) -> torch.Tensor:
-        if self.hparams.uncertainty_type == 'msp':
+        if self.hparams.uncertainty_type == "msp":
             softmax_probs = torch.softmax(logits_known, dim=1)
             max_probs, _ = torch.max(softmax_probs, dim=1)
             anomaly_scores = 1.0 - max_probs
-        elif self.hparams.uncertainty_type == 'max_logit':
+        elif self.hparams.uncertainty_type == "max_logit":
             max_logits, _ = torch.max(logits_known, dim=1)
             anomaly_scores = -max_logits
-        elif self.hparams.uncertainty_type == 'entropy':
+        elif self.hparams.uncertainty_type == "entropy":
             softmax_probs = torch.softmax(logits_known, dim=1)
             entropy = -torch.sum(softmax_probs * torch.log(softmax_probs + 1e-9), dim=1)
             anomaly_scores = entropy
@@ -51,14 +51,14 @@ class UncertaintyModel(BaseSemanticSegmentationModel):
 
         images, gt_full_masks = batch
         outputs = self(images)
-        logits_known = outputs['out']
+        logits_known = outputs["out"]
 
         gt_anomaly_masks = (gt_full_masks == ANOMALY_ID).long()
         anomaly_scores = self._calculate_anomaly_scores(logits_known)
 
         pixel_anomaly_scores = anomaly_scores.detach().cpu().numpy().ravel()
         pixel_anomaly_labels = gt_anomaly_masks.detach().cpu().numpy().ravel()
-        
+
         self.all_pixel_anomaly_scores.append(pixel_anomaly_scores)
         self.all_pixel_anomaly_labels.append(pixel_anomaly_labels)
 
@@ -68,4 +68,4 @@ class UncertaintyModel(BaseSemanticSegmentationModel):
 
         aupr = average_precision_score(all_labels, all_scores)
 
-        self.log('test_aupr_anomaly', aupr, prog_bar=True, logger=True)
+        self.log("test_aupr_anomaly", aupr, prog_bar=True, logger=True)
