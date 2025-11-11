9728123a167c2beda13cb1aa024fbd9bf8ea4512
Alessio Pittiglio <alessiopittiglio@icloud.com>
2025-11-11 22:19:23 +0100

    Reformat transform.py using Black

--- mlcv_openset_segmentation/transforms.py
+++ mlcv_openset_segmentation/transforms.py
@@ -1,14 +1,17 @@
 from torchvision.transforms import v2
 import torch
 
+
 def get_transforms(cfg: dict):
-    transform = v2.Compose([
-        v2.Resize(
-            (cfg['img_height'], cfg['img_width']),
-            interpolation=v2.InterpolationMode.BILINEAR
-        ),
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(mean=cfg['img_mean'], std=cfg['img_std']),
-    ])
+    transform = v2.Compose(
+        [
+            v2.Resize(
+                (cfg["img_height"], cfg["img_width"]),
+                interpolation=v2.InterpolationMode.BILINEAR,
+            ),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=cfg["img_mean"], std=cfg["img_std"]),
+        ]
+    )
     return transform, transform
