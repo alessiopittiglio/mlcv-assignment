4816168fa60948b66f23d81ddb58e38d719a18f1
Alessio Pittiglio <alessiopittiglio@icloud.com>
2025-11-25 02:43:25 +0100

    Add Energy-Entropy loss

--- mlcv_openset_segmentation/rpl_losses.py
+++ mlcv_openset_segmentation/rpl_losses.py
@@ -58,3 +58,66 @@ def energy_loss(logits, targets, vanilla_logits, out_idx, t=1.0):
     outlier_loss = energy_part * 0.05
 
     return inlier_loss + outlier_loss
+
+
+def energy_entropy_loss(
+    logits: torch.Tensor,
+    targets: torch.Tensor,
+    vanilla_logits: torch.Tensor,
+    out_idx: int,
+    t: float = 1.0,
+    alpha: float = 1.0,
+) -> torch.Tensor:
+    """
+    Energy-Entropy Loss.
+
+    Adapted from:
+    Song Xia et al., "Open-set Anomaly Segmentation in Complex Scenarios".
+    """
+
+    out_msk = targets == out_idx
+    void_msk = targets == 255
+
+    pseudo_targets = torch.argmax(vanilla_logits, dim=1)
+    outlier_msk = out_msk | void_msk
+
+    entropy_part = F.cross_entropy(logits, pseudo_targets, reduction="none")[
+        ~outlier_msk
+    ]
+
+    reg = disimilarity_entropy(logits, vanilla_logits)[~outlier_msk]
+
+    if torch.sum(out_msk) > 0:
+
+        eps = 1e-6
+        prob = (
+            torch.clamp(torch.softmax(logits, dim=1), eps)
+            .flatten(start_dim=2)
+            .permute(0, 2, 1)
+        )
+        logits_flat = logits.flatten(start_dim=2).permute(0, 2, 1)
+
+        energy = torch.logsumexp(logits_flat, dim=2)
+        entropy = -torch.sum(prob * torch.log(prob), dim=2)
+
+        sigmoid_energy = torch.clamp(torch.sigmoid(-energy), eps, 1 - eps)
+
+        outlier_flat = out_msk.flatten(start_dim=1)
+        inlier_flat = ~outlier_flat
+
+        outlier_part = (
+            -torch.log(sigmoid_energy[outlier_flat]) - alpha * entropy[outlier_flat]
+        )
+        inlier_part = (
+            torch.log(1 - sigmoid_energy[inlier_flat]) + alpha * entropy[inlier_flat]
+        )
+
+        energy_entropy = outlier_part.mean() - inlier_part.mean()
+
+    else:
+        energy_entropy = torch.tensor(0.0, device=targets.device)
+
+    inlier_loss = entropy_part.mean() + reg.mean()
+    outlier_loss = energy_entropy * 0.05
+
+    return inlier_loss + outlier_loss
