e4fca7a1c942c7cc936cec4d47744fe869404007
Alessio Pittiglio <alessiopittiglio@icloud.com>
2025-11-26 04:14:49 +0100

    Remove unused import of torch.nn.functional from postprocessing.py

--- mlcv_openset_segmentation/postprocessing.py
+++ mlcv_openset_segmentation/postprocessing.py
@@ -10,7 +10,6 @@ import math
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from kornia.morphology import dilation, erosion
 from scipy import ndimage as ndi
 
